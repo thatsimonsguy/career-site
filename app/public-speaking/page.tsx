8481dff3c7e3ab5c7bd22c232196fd28f24a833a
Matt Simons <thatsimonsguy@gmail.com>
2025-04-28 19:28:59 -0500

    added under construction pages

---
+++ app/public-speaking/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+import Image from "next/image";
+
+export default function PublicSpeakingPage() {
+    return (
+        <>
+            <Header />
+            <main className="min-h-screen flex flex-col items-center justify-center text-center p-8">
+                <h1 className="text-4xl font-bold text-red-900 mb-6">Public Speaking</h1>
+                <Image
+                    src="images/DSC_3058.JPG"
+                    alt="Under construction"
+                    width={500}
+                    height={500}
+                    className="mb-6"
+                />
+                <p className="text-gray-700 text-lg">
+                    🚧 Matt’s still building this page.
+                    <br></br>In the meantime, please enjoy this picture of a sunbathing dog.
+                </p>
+            </main>
+            <Footer />
+        </>
+    );
+}
